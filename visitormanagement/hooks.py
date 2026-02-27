9dafb8fc974bdedaf6410f95f0f5723151a36dc7
Harthesh <hharthesh@gmail.com>
2026-02-27 13:00:44 +0530

    worked in workflow

--- visitormanagement/hooks.py
+++ visitormanagement/hooks.py
@@ -247,3 +247,20 @@ app_license = "mit"
 # List of apps whose translatable strings should be excluded from this app's translations.
 # ignore_translatable_strings_from = []
 
+fixtures = [
+    {
+        "doctype": "Workflow",
+        "filters": [["name", "in", [
+            "CRITICAL RISK RISK",
+            "HIGH RISK RISK",
+            "MEDIUM RISK RISK",
+            "LOW RISK RISK"
+        ]]]
+    },
+    {
+        "doctype": "Workflow State"
+    },
+    {
+        "doctype": "Workflow Transition"
+    }
+]
